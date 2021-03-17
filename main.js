55e7d210032930efda67d3abb4e0467cc0fbaf63
Piotr Krzaczkowski <slimshade93@wp.pl>
2021-03-17 22:09:39 +0100

    connection line fixed

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
 window.addEventListener("load", () => {
   const canvas = document.querySelector("#canvas");
   const context = canvas.getContext("2d");
+
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+
+  let painting = false;
+
+  function startPosition(e) {
+    painting = true;
+    draw(e);
+  }
+
+  function finishedPosition() {
+    painting = false;
+    context.beginPath();
+  }
+
+  const draw = (e) => {
+    if (!painting) return;
+
+    context.lineWidth = 10;
+    context.lineCap = "round";
+
+    context.lineTo(e.clientX, e.clientY);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(e.clientX, e.clientY);
+  };
+
+  canvas.addEventListener("mousedown", startPosition);
+  canvas.addEventListener("mouseup", finishedPosition);
+  canvas.addEventListener("mousemove", draw);
 });
